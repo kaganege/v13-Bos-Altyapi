0be5a60b1728b674e3e8f10792bde6c5977aed92
Tavuk Döner <79661973+TavukDoner7528@users.noreply.github.com>
2021-08-06 22:17:46 +0300

    Update ping.js

--- Slash-Commands/ping.js
+++ Slash-Commands/ping.js
@@ -2,13 +2,10 @@ module.exports = {
     name: "ping",
     description: "Botun pingini gösterir",
     async execute(interaction, client) {
-        const reply = await interaction.reply({ content: "Ping Ölçülüyor..." });
-        console.log(reply);
-        // var ping = reply.createdTimestamp - interaction.createdTimestamp;
-        // await interaction.editReply(`
-        //     :ping_pong: Pong!
-        //     > Bot: ${ping} ms
-        //     > Discord: ${client.ws.ping} ms
-        //     `);
+        await interaction.reply({ content: "Ping Ölçülüyor..." });
+        await interaction.editReply(`
+            :ping_pong: Pong!
+            > ${client.ws.ping} ms
+            `);
     },
 };
