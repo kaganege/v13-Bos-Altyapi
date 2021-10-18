3ceab8632b8d5b4e67e0c745f7f6867f0bfbb8ef
Tavuk Döner <79661973+TavukDoner7528@users.noreply.github.com>
2021-10-18 22:04:06 +0300

    Kod kısaltıldı

--- index.js
+++ index.js
@@ -19,70 +19,16 @@ const client = new Client({
   ],
   partials: ["MESSAGE", "CHANNEL", "REACTION"],
 });
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const fs = require("fs");
 const AsciiTable = require("ascii-table");
 const { sahip, prefix } = require("./config.json");
 const token = process.env["token"]
-const rest = new REST({ version: '9' }).setToken(token);
 
 client.slashCommmands = new Collection();
 client.commands = new Collection();
 
-const eventFiles = fs
-  .readdirSync("./events")
-  .filter((file) => file.endsWith(".js"));
-
-for (const file of eventFiles) {
-  const event = require(`./events/${file}`);
-  if (event.once) {
-    client.once(event.name, (...args) =>
-      event.execute(...args, client, Discord)
-    );
-  } else {
-    client.on(event.name, (...args) =>
-      event.execute(...args, client, Discord)
-    );
-  }
-}
-
-const commandFiles = fs
-  .readdirSync("./Commands")
-  .filter((file) => file.endsWith(".js"));
-
-console.log(`${commandFiles.length} Komut Yüklenecek!`);
-
-commandFiles.forEach((file) => {
-  const command = require(`./Commands/${file}`);
-  console.log("Yüklenen komut: " + command.name);
-  client.commands.set(command.name, command);
-});
-
-const slashCommandFiles = fs
-  .readdirSync("./Slash-Commands")
-  .filter((file) => file.endsWith(".js"));
-
-console.log(`${slashCommandFiles.length} Slash Komutu Yüklenecek!`);
-
-var data = []
-for (const file of slashCommandFiles) {
-  const command = require(`./Slash-Commands/${file}`);
-  console.log("Yüklenen slash komutu: " + command.name);
-  client.slashCommmands.set(command.name, command);
-  data.push(new SlashCommandBuilder().setName(command.name).setDescription(command.description))
-}
-
-data.map(cmd => cmd.toJSON())
-
-setTimeout(async () => {
-  rest.put(Routes.applicationCommands(client.user.id), {body: data})
-  .then(() => console.log('Slash Komutları kuruldu!'))
-	.catch(console.error);
-}, 500);
-
 client.login(token);
+
+require('./handlers')(client);
 
 Promise.prototype.sil = function(time) {
   if (this)
